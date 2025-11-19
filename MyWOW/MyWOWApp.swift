c2035a028e9c87ec292f84ca5e867eb27682a549
O-Leon <78105710+O-Leon@users.noreply.github.com>
2025-11-18 22:38:58 -0600

    Initial Commit

---
+++ MyWOW/MyWOWApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWOWApp.swift
+//  MyWOW
+//
+//  Created by Omer on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct MyWOWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
